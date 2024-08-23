9d11a0777d41ed34cbca1ce30830e555d3f94ce2
Gonzalo Rios <gonzarios95@gmail.com>
2024-08-23 21:06:13 +0200

    realizo el contador de ejemplo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
+import "./App.css";
+import Contador from "./assets/components/Contador";
+import Informacion from "./assets/components/Informacion";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import './App.css'
-import Contador from './assets/components/Contador'
-import Informacion from './assets/components/Informacion'
-import 'bootstrap/dist/css/bootstrap.min.css'
-function App() {
- 
+function App() { 
+  
+  const clasico = "Atletico Tucuman";
+  const santo = "San Martin de Tucuman";
 
+
+
+  
+  
   return (
     <>
-      
-      <Informacion/>
-      <Contador/>
+      <Informacion miEquipo={santo} clasicoActual={clasico} />
+      <Contador  />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/assets/components/Contador.jsx
+++ src/assets/components/Contador.jsx
@@ -1,6 +1,34 @@
+
+import { useState } from "react";
+
+
+// eslint-disable-next-line react/prop-types
 const Contador = () => {
-  return (
-    <section><h2>Contador</h2></section>
-  )
+    //aqui va la logica de una funcion si quiero
+    const [adicion, setAdicion]= useState(0);
+    const [sustraccion,setSustraccion]= useState(0)
+    const sumar =()=>{
+setAdicion (adicion + 1) 
+
+  }
+const sumarNO =() => {
+
+    setSustraccion (sustraccion + 1)
+
+
 }
-export default Contador
+  return (
+    <section>
+      <h2>Contador</h2>
+      <article className="d-flex justify-content-evenly">
+        <button className="btn btn-success" onClick={sumar}>Me gusta<p className="mt-1 mb-1">{adicion}</p></button>
+        <button className="btn btn-danger" onClick={sumarNO}>No me gusta<p className="mt-1 mb-1">{sustraccion}</p></button>
+      </article>
+ 
+    </section>
+
+  );
+};
+
+
+export default Contador;
